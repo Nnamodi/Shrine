7e9204bc277b2e61f3a7ba3601b4212d227ffa55
Roland <rolandnnamodi@gmail.com>
2023-01-03 20:10:48 +0100

    Improved UX with some animation

--- app/src/main/java/com/roland/android/shrine/BackDrop.kt
+++ app/src/main/java/com/roland/android/shrine/BackDrop.kt
@@ -1,34 +1,58 @@
 package com.roland.android.shrine
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.roland.android.shrine.ui.theme.ShrineTheme
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 val menuData = listOf("Featured", "Apartment", "Accessories", "Shoes", "Tops", "Bottoms", "Dresses")
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 @Composable
 fun BackDrop() {
     val scope = rememberCoroutineScope()
     var menuSelection by remember { mutableStateOf(0) }
-    val backdropState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    var backdropRevealed by remember { mutableStateOf(scaffoldState.isRevealed) }
 
     BackdropScaffold(
         appBar = {
-            TopAppBar(scope, backdropState)
+            TopAppBar(
+                backdropRevealed = backdropRevealed,
+                onBackdropReveal = {
+                    scope.launch {
+                        if (scaffoldState.isConcealed) {
+                            scaffoldState.reveal()
+                        } else {
+                            scaffoldState.conceal()
+                        }
+                    }
+                    backdropRevealed = it
+                }
+            )
         },
         backLayerContent = {
             BackdropMenuItem(
@@ -50,40 +74,70 @@ fun BackDrop() {
             }
         },
         frontLayerShape = MaterialTheme.shapes.large,
-        scaffoldState = backdropState
+        scaffoldState = scaffoldState
     )
 }
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 @Composable
 private fun TopAppBar(
-    scope: CoroutineScope,
-    backdropState: BackdropScaffoldState,
+    backdropRevealed: Boolean,
+    onBackdropReveal: (Boolean) -> Unit = {},
 ) {
     TopAppBar(
         title = {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            val density = LocalDensity.current
+
+            Box(
+                Modifier
+                    .width(46.dp)
+                    .fillMaxHeight()
+                    .toggleable(
+                        value = backdropRevealed,
+                        onValueChange = { onBackdropReveal(it) },
+                        indication = rememberRipple(bounded = false, radius = 56.dp),
+                        interactionSource = remember { MutableInteractionSource() }
+                    ),
+                contentAlignment = Alignment.CenterStart
             ) {
+                AnimatedVisibility(
+                    visible = !backdropRevealed,
+                    enter = fadeIn(animationSpec = tween(100, 90, LinearEasing))
+                            + slideInHorizontally(initialOffsetX = { with(density) { (-20).dp.roundToPx() } }, animationSpec = tween(durationMillis = 270, easing = LinearEasing)),
+                    exit = fadeOut(animationSpec = tween(durationMillis = 120, easing = LinearEasing))
+                            + slideOutHorizontally(targetOffsetX = { with(density) { (-20).dp.roundToPx() } }, animationSpec = tween(durationMillis = 120, easing = LinearEasing))
+                ) {
+                    Icon(
+                        painterResource(id = R.drawable.menu),
+                        contentDescription = "Menu navigation icon"
+                    )
+                }
+
+                val logoTransition = updateTransition(
+                    targetState = backdropRevealed,
+                    label = "logoTransition"
+                )
+                val logoOffset = logoTransition.animateDp(
+                    transitionSpec = {
+                        val millis = if (targetState) 120 else 270
+                        tween(durationMillis = millis, easing = LinearEasing)
+                    },
+                    label = "logoOffset"
+                ) { revealed ->
+                    if (revealed) 0.dp else 20.dp
+                }
                 Icon(
-                    painterResource(id = R.drawable.menu),
-                    contentDescription = "Menu icon",
-                    modifier = Modifier.clickable {
-                        scope.launch {
-                            if (backdropState.isConcealed) {
-                                backdropState.reveal()
-                            } else {
-                                backdropState.conceal()
-                            }
-                        }
-                    }
+                    painterResource(id = R.drawable.logo),
+                    contentDescription = "Shrine logo",
+                    modifier = Modifier.offset(x = logoOffset.value)
                 )
-                Spacer(Modifier.width(6.dp))
-                Text(
-                    "Shrine".uppercase(),
-                    style = MaterialTheme.typography.subtitle1,
-                    fontSize = 17.sp
-                )
+            }
+
+            if (!backdropRevealed) {
+                TopAppBarText(text = "Shrine")
+            } else {
+                MenuSearchField()
             }
         },
         actions = {
@@ -95,6 +149,50 @@ private fun TopAppBar(
         },
         elevation = 0.dp
     )
+}
+
+@Composable
+private fun TopAppBarText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text.uppercase(),
+        modifier = modifier,
+        style = MaterialTheme.typography.subtitle1,
+        fontSize = 17.sp
+    )
+}
+
+@Composable
+fun MenuSearchField() {
+    var searchText by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .height(56.dp)
+            .padding(end = 12.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        BasicTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            singleLine = true,
+            decorationBox = { innerTextField ->
+                Row(
+                    Modifier
+                        .padding(end = 36.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) { innerTextField() }
+            }
+        )
+        if (searchText.isEmpty()) {
+            TopAppBarText(
+                modifier = Modifier.alpha(ContentAlpha.disabled),
+                text = "Search Shrine"
+            )
+        }
+    }
 }
 
 @Composable
@@ -157,6 +255,7 @@ private fun MenuItem(
     }
 }
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 @Preview
 @Composable
--- app/src/main/java/com/roland/android/shrine/MainActivity.kt
+++ app/src/main/java/com/roland/android/shrine/MainActivity.kt
@@ -3,11 +3,13 @@ package com.roland.android.shrine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.roland.android.shrine.ui.theme.ShrineTheme
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
